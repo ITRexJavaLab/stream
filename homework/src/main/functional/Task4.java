2cb06d522c8cda88387664216db1a9045b0240e3
Pavel Tsekhanovich <contr.pavel.tsekhanovich@pandadoc.com>
2021-09-20 14:05:36 +0300

    Update task.

--- homework/src/main/functional/Task4.java
+++ homework/src/main/functional/Task4.java
@@ -2,13 +2,9 @@ package main.functional;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Write a collector that evaluates the product of squares of integer numbers in a Stream<Integer>.
- * <p>
- * Important. You should write only the collector in any valid formats but without ; on the end.
- * It will be passed as an argument to the collect() method of a stream as shown below.
  */
 public class Task4 {
 
